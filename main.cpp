47ee259a7f25aeab53050f4e279c56d76959eda9
Charles Schiermeyer <matkatmusic@gmail.com>
2020-03-28 14:57:25 -0700

    Revised instructions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ Project 5: Part 1 / 4
 
  Create a branch named Part1
 
+Purpose:  This project continues developing Project3 and will take you through how to write code that doesn't leak as well as how to refactor. 
+
  Destructors
+
+ If you didn't do Project3, write 3 UDTs below (where it asks for Copied UDTs) that have: 
+        member functions,
+        member variables, 
+        constructors that initialize the members. 
+        for() or while() loops that modify member variables
+        
+ Define your implementations of all functions OUTSIDE of the class. 
+ Then do instruction 2)
+ 
+ If you DID do Project 3, do instruction 1)
  
  1) Copy a few of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
  2) add destructors
@@ -16,13 +29,18 @@ Project 5: Part 1 / 4
  4) Add destructors to these 2 new types that do stuff.  
         maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
  
- 5) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+ 5) copy over your main() from the end of Project3 and get it to compile with the types you copied over.
+        remove any code in main() that uses types you didn't copy over.
  
- 6) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 6) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
+ 
+ 7) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- Send me the the link to your repl.it in a DM on Slack
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
 
  Wait for my code review.
  */
