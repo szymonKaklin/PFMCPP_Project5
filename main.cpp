bf4899bd3331f4bccacffe07087b032349d4463f
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2020-09-10 11:25:07 +0000

    Completed Part1

--- main.cpp
+++ main.cpp
@@ -32,27 +32,213 @@ Purpose:  This project continues developing Project3.
  
  */
 
+#include <iostream>
 /*
  copied UDT 1:
  */
+struct Pizza
+{
+    Pizza();
+    ~Pizza() { std::cout << "Destroying Pizza" << std::endl; }
+    int numOfToppings;
+    int diameter;
+    double sliceSize;
+    double calculateSliceSize(int slices);
+    Pizza makePizza();
+    void distributeToppings(int toppings, int slices);
+};
+
+Pizza::Pizza() : numOfToppings(1), diameter(14), sliceSize(4.5) {}
+
+double Pizza::calculateSliceSize(int slices)
+{
+    return diameter / (sliceSize * slices);
+}
+
+Pizza Pizza::makePizza()
+{
+    Pizza pepperoni;
+    std::cout << "Pizza made!" << std::endl;
+    return pepperoni;
+}
+
+void Pizza::distributeToppings(int toppings, int slices)
+{
+    int tps = toppings/slices;
+    for( int i = 0; i < slices; ++i )
+    {
+        std::cout << "Added " << tps << " toppings to slice" << std::endl;
+    }
+    int leftover = toppings % slices;
+    std::cout << "Leftover toppings: " << leftover << std::endl;
+    numOfToppings -= 1;
+    std::cout << "Toppings left to distribute: " << numOfToppings << std::endl;
+}
+
 
 /*
  copied UDT 2:
  */
+struct Guitar
+{
+    Guitar();
+    ~Guitar() { std::cout << "Destroying Guitar" << std::endl; }
+    int stringNum = 7;
+    int fretNum = 24;
+    char model = 'i';
+    bool tuned = false;
+
+    struct Strings
+    {
+        Strings();
+        double gauge;
+    };
+
+    void restringGuitar(Strings strings);
+    void tune();
+};
+
+Guitar::Guitar() {}
+Guitar::Strings::Strings()
+{
+    gauge = 0.011;
+}
+
+void Guitar::restringGuitar(Guitar::Strings strings)
+{
+    std::cout << "Guitar of model " << model << ", with string number: "<< stringNum << ", has been   re-strung using strings of gauge: "<< strings.gauge << "\n" << std::endl;
+}
+
+void Guitar::tune()
+{
+    std::cout << "\nTuning Guitar..." << std::endl;
+    for ( int i = 0; i < stringNum; ++i )
+    {
+        std::cout << "Tuned string " << i+1 << std::endl;
+    }
+    tuned = true;
+    std::cout << "Guitar Tuned!" << std::endl;
+}
 
 /*
  copied UDT 3:
  */
+struct Airliner
+{
+    Airliner();
+    ~Airliner() { std::cout << "Destroying Airliner" << std::endl; }
+    int engines = 2;
+    int capacity = 416;
+    int fuelCapacity = 183380;
+    char company = 'b';
+    void checkFuelAmount();
+    bool isTakeoffReady(double fuelAmount);
+    int beginBoarding(int passengers);
+    
+};
+
+Airliner::Airliner() {}
+
+void Airliner::checkFuelAmount()
+{
+    std::cout << "Fuel amount: " << fuelCapacity << "\n" << std::endl;
+}
+
+bool Airliner::isTakeoffReady(double fuelAmount)
+{
+    return fuelAmount > fuelCapacity;
+}
+
+int Airliner::beginBoarding(int passengers)
+{
+    while( passengers > 0 )
+    {
+        --capacity;
+        if( capacity < 0 )
+        {
+            std::cout << "Plane Full! Leftover Passengers: " << passengers << std::endl;
+            return 0;
+        }
+        --passengers;
+    }
+    std::cout << "Boarding Complete! Empty Seats: " << capacity << std::endl;
+    return 0;
+}
 
 /*
  new UDT 4:
  with 2 member functions
  */
 
+struct PizzaPlane 
+{
+    PizzaPlane()
+    {
+        std::cout << "Constructing PizzaPlane with pizza of slice size " << pizza.sliceSize << " and plane of type " << plane.company << std::endl;
+    }
+    ~PizzaPlane()
+    {
+        std::cout << "Destroying PizzaPlane" << std::endl;
+    }
+
+    Pizza pizza;
+    Airliner plane;
+
+    void loadPizza();
+    double calculatePizzaPerPerson(); 
+};
+
+void PizzaPlane::loadPizza()
+{
+    std::cout << "loading pizza of diameter " << pizza.diameter << " on plane of type " << plane.company << std::endl;
+}
+
+double PizzaPlane::calculatePizzaPerPerson()
+{
+    double r = pizza.diameter/2;
+    return (r*r*3.1415)/plane.capacity;
+}
+
 /*
  new UDT 5:
  with 2 member functions
  */
+
+struct Party
+{
+    Party()
+    {
+        std::cout << "Constructing Party for a maximum of " << plane.capacity << " people" << std::endl;
+    }
+    ~Party()
+    {
+        std::cout << "Destroying Party" << std::endl;
+    }
+    
+    Pizza pizza;
+    Airliner plane;
+
+    void addToppingsPerString(Guitar guitar);
+    void startParty(double fuel, Guitar guitar);
+};
+
+void Party::addToppingsPerString(Guitar guitar)
+{
+    pizza.numOfToppings += guitar.stringNum;
+    std::cout << "Added " << guitar.stringNum << " toppings" << std::endl;
+}
+
+void Party::startParty(double fuel, Guitar guitar)
+{
+    if( plane.isTakeoffReady(fuel) && guitar.tuned)
+    {
+        std::cout << "Party can start! Guitar tuned and fuel amount is OK" << std::endl;
+    }
+    else
+    {
+        std::cout << "not enough preperations" << std::endl;
+    }
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -67,9 +253,50 @@ Purpose:  This project continues developing Project3.
 
  Wait for my code review.
  */
-
-#include <iostream>
 int main()
 {
-    std::cout << "good to go!" << std::endl;
+    std::cout << "good to go!\n" << std::endl;
+    
+    Guitar gibson;
+    Guitar::Strings ernie;
+    gibson.restringGuitar(ernie);
+
+    Airliner airbus737;
+    airbus737.checkFuelAmount();
+    if(airbus737.isTakeoffReady(500000))
+    {
+        std::cout << "Is takeoff ready \n" << std::endl;
+    }
+    else
+    {
+        std::cout << "Is not takeoff ready \n" << std::endl;
+    }
+
+    Pizza margharita;
+    std::cout << "Slice size is: " << margharita.calculateSliceSize(6) << "\n" << std::endl;
+
+
+    // Project 3 Part 5 Checks
+    std::cout << "\nProject 3 Part 5 Checks\n" << std::endl;
+
+    gibson.tune();
+
+    std::cout << "\n" << std::endl;
+    airbus737.beginBoarding(420);
+
+    std::cout << "\n" << std::endl;
+    margharita.distributeToppings(35, 8);
+
+    // Project 5 Part 1
+    std::cout << "\nProject 5 Part 1 Checks\n" << std::endl;
+
+    PizzaPlane order;
+    order.loadPizza();
+
+    std::cout << "Pizza per person with max passanger count: " << order.calculatePizzaPerPerson() << std::endl;
+
+    Party party;
+    party.addToppingsPerString(gibson);
+    party.startParty(183390, gibson);
 }
+
